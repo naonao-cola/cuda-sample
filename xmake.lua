7eb47744d47fcd9a8088855a14d446916e008365
naonao <1319144981@qq.com>
2024-09-23 15:18:21 +0800

    first commit

---
+++ xmake.lua
@@ -0,0 +1,35 @@
+set_project("CUDA-SAMPLE")
+set_version("1.1.0")
+set_languages("c++17")
+
+
+add_rules("mode.release", "mode.debug", "mode.check","mode.releasedbg")
+set_runtimes("MT")
+
+tutorial_list = {
+        "01",
+        "02",
+        "03",
+        "04",
+}
+
+for _, v in pairs(tutorial_list) do
+    target(v)
+        set_kind("binary")
+        add_rules("cuda")
+        add_cugencodes("native")
+        add_cuflags("-allow-unsupported-compiler")
+        set_group("test")
+        add_headerfiles("src/common/common.h")
+        add_rpathdirs("$(buildir)/windows/x64/releasedbg/")
+        for _, filedir in ipairs(os.filedirs(string.format("src/%s/**", v))) do
+            --print(filedir)
+            local s = filedir
+            if s:endswith(".cuh") or s:endswith(".h") then
+                 add_headerfiles(filedir)
+            end
+            if s:endswith(".cu") or s:endswith(".cpp") then
+                add_files(filedir)
+            end
+        end
+end
